51609cb984063fe87c18389891d46ec148844435
Evan Kohout <ebox86@gmail.com>
2022-09-28 23:56:57 -0400

    added shuttle deployment support and command structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,44 +1,108 @@
+mod commands;
+use serenity::model::prelude::command::CommandOptionType;
+
+use std::env;
+use serenity::model::application::command::Command;
 use log::{error, info};
-use serenity::async_trait;
-use serenity::model::channel::Message;
+use serenity::model::application::interaction::{Interaction, InteractionResponseType};
 use serenity::model::gateway::Ready;
 use serenity::prelude::*;
+use serenity::{async_trait, model::prelude::GuildId};
 use shuttle_service::error::CustomError;
 use shuttle_service::SecretStore;
 use sqlx::PgPool;
 
-struct Bot;
-
-#[async_trait]
-impl EventHandler for Bot {
-    async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "!hello" {
-            if let Err(e) = msg.channel_id.say(&ctx.http, "world!").await {
-                error!("Error sending message: {:?}", e);
-            }
-        }
-    }
-
-    async fn ready(&self, _: Context, ready: Ready) {
-        info!("{} is connected!", ready.user.name);
-    }
+struct Bot {
+    client: reqwest::Client,
+	discord_guild_id: GuildId,
 }
 
 #[shuttle_service::main]
 async fn serenity(#[shared::Postgres] pool: PgPool) -> shuttle_service::ShuttleSerenity {
     // Get the discord token set in `Secrets.toml` from the shared Postgres database
-    let token = pool
+    let discord_token = pool
         .get_secret("DISCORD_TOKEN")
         .await
         .map_err(CustomError::new)?;
 
-    // Set gateway intents, which decides what events the bot will be notified about
-    let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
-
-    let client = Client::builder(&token, intents)
-        .event_handler(Bot)
+    let discord_guild_id = pool
+        .get_secret("DISCORD_GUILD_ID")
         .await
-        .expect("Err creating client");
+        .map_err(CustomError::new)?;
+
+    let client = get_client(
+        &discord_token,
+        discord_guild_id.parse().unwrap(),
+    )
+    .await;
 
     Ok(client)
 }
+
+pub async fn get_client(
+    discord_token: &str,
+    discord_guild_id: u64,
+) -> Client {
+    // Set gateway intents, which decides what events the bot will be notified about.
+    // Here we don't need any intents so empty
+    let intents = GatewayIntents::empty();
+
+    Client::builder(discord_token, intents)
+        .event_handler(Bot {
+            client: reqwest::Client::new(),
+            discord_guild_id: GuildId(discord_guild_id),
+        })
+        .await
+        .expect("Err creating client")
+}
+
+#[async_trait]
+impl EventHandler for Bot {
+    async fn ready(&self, ctx: Context, ready: Ready) {
+        info!("{} is connected!", ready.user.name);
+
+        let commands = GuildId::set_application_commands(&self.discord_guild_id, &ctx.http, |commands| {
+            commands
+                .create_application_command(|command| commands::ping::register(command))
+                .create_application_command(|command| commands::id::register(command))
+                .create_application_command(|command| commands::welcome::register(command))
+                .create_application_command(|command| commands::numberinput::register(command))
+                .create_application_command(|command| commands::attachmentinput::register(command))
+        })
+        .await;
+
+        println!("I now have the following guild slash commands: {:#?}", commands);
+
+        let guild_command = Command::create_global_application_command(&ctx.http, |command| {
+            commands::wonderful_command::register(command)
+        })
+        .await;
+
+        println!("I created the following global slash command: {:#?}", guild_command);
+    }
+
+    async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
+        if let Interaction::ApplicationCommand(command) = interaction {
+            println!("Received command interaction: {:#?}", command);
+
+            let content = match command.data.name.as_str() {
+                "ping" => commands::ping::run(&command.data.options),
+                "id" => commands::id::run(&command.data.options),
+                "attachmentinput" => commands::attachmentinput::run(&command.data.options),
+                _ => "not implemented :(".to_string(),
+            };
+
+            if let Err(why) = command
+                .create_interaction_response(&ctx.http, |response| {
+                    response
+                        .kind(InteractionResponseType::ChannelMessageWithSource)
+                        .interaction_response_data(|message| message.content(content))
+                })
+                .await
+            {
+                println!("Cannot respond to slash command: {}", why);
+            }
+        }
+    }
+}
+
