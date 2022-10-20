c19d7a7f3780dcf7e24839b0a15e8610fefca385
Evan Kohout <ebox86@gmail.com>
2022-10-20 08:21:59 -0400

    added dropdown for guildid on rank page

--- web/pages/Rank/index.vue
+++ web/pages/Rank/index.vue
@@ -1,7 +1,14 @@
 <template>
     <NuxtLayout>
-        <div>
-            <va-data-table :items="ranks" />
+        <div class="lists">
+            <va-select
+                class="mb-4"
+                label="Guild Id"
+                :options="options"
+                v-model="options"
+                loading
+            />
+            <va-data-table :items="ranks" v-model="value" :options="options" track-by="id" />
             <!-- <Rank v-for="rank in ranks" :key="rank.user_id" 
             :user_id="rank.user_id" :points="rank.points" :level="rank.level" :user_name="rank.user_name"/> -->
         </div>
@@ -16,8 +23,17 @@ export default {
         Rank
     },
     data() {
+    const options = [
+      {
+        text: '698169764861837372',
+        value: '698169764861837372',
+        id: '1',
+      },
+    ]
         return {
-            ranks: []
+            ranks: [],
+            value: options[0].value,
+            options,
         }
     },
     async created(){
@@ -27,7 +43,7 @@ export default {
             }
         }
         try {
-            const response = await useFetch("https://discord-fank.shuttleapp.rs/rank/show")
+            const response = await useFetch(`https://discord-fank.shuttleapp.rs/rank/show/${this.value}`)
             this.ranks = response.data;
         } catch (error) {
             console.log(error)
@@ -36,3 +52,11 @@ export default {
 }
 
 </script>
+
+<style>
+.lists {
+    padding-top: 1rem;
+    width: 100%;
+    height: 100%;
+}
+</style>
